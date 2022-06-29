b6e20d7cc51254a88c287dcc874f65fca71dea43
arpan404 <itsmearpan1@gmail.com>
2022-06-29 16:43:40 +0545

    noresults refactorized

--- src/components/noresults.js
+++ src/components/noresults.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import NoResultImage from'./assets/noresults.svg'
-export default class NoResults extends Component {
-  render() {
-    return (
-      <div className="text-center my-3">
-        <h3 style ={{color:'#545454'}}>
-        No Results Found {this.props.query?"for '"+this.props.query+"'":''}
-        </h3>
-        <img src={NoResultImage} style={{maxWidth:'100%',height: 'calc(100vh - 100px)'} }/>
-      </div>
-
-    )
-  }
+import React from "react";
+import NoResultImage from "./assets/noresults.svg";
+export default function NoResults() {
+  return (
+    <div className="text-center my-3">
+      <h3 style={{ color: "#545454" }}>
+        No Results Found{" "}
+        {this.props.query ? "for '" + this.props.query + "'" : ""}
+      </h3>
+      <img
+        src={NoResultImage}
+        style={{ maxWidth: "100%", height: "calc(100vh - 100px)" }}
+      />
+    </div>
+  );
 }
